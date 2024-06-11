f2acd17a805d939bc7a6c69fe7d567c3676108e0
Siji <sijijoseph95@gmail.com>
2024-06-11 15:14:36 +1200

    task system

---
+++ README.md
@@ -0,0 +1,3 @@
+#Task System
+
+simple laravel application to manage and organize tasks efficiently. This application allows users to create, update, delete, and view tasks, set deadlines and show completed tasks.
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks/store', [TaskController::class, 'store'])->name('tasks.store');
+
+Route::get('/tasks/{task}', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::post('/tasks/{task}/complete', [TaskController::class, 'complete'])->name('tasks.complete');
+
+Route::get('/taskshow', [TaskController::class, 'showCompleted'])->name('taskshow');
